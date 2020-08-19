84998c9f60e931c15c7c29fcd285186c08404da9
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-08-19 14:00:05 -0700

    setting up layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Portfolio from "./components/pages/Portfolio";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
-class App extends Component {
 
-
+function App() {
+    return (
+        <Router>
+          <div>
+            <NavTabs />
+            <Route exact path="/" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+          </div>
+        </Router>
+    );
 }
 
 export default App;
