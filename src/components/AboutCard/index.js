d11ffa3f6ac300a6476a6e8dab7494ccc0f7c7eb
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-08-23 12:18:06 -0700

    images work

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -8,7 +8,7 @@ return (
     <div className="row justify-content-md-center">
         <Container id="about" className="col-md-8 col-sm-12 col-lg-10">
             <h1> About Me </h1>
-            <img className="meimg" alt= "Kaelyn Rieffer" src= {"../UsedImgs/IMG_20180226_202959_616.jpg"} />
+            <img className="meimg" alt= "Kaelyn Rieffer" src="https://drive.google.com/uc?export=view&id=1m-_9-569U4uKI2G4coEj9bjtrIlL_rni"/>
             <p className="meText"> I am an artist and aspiring web developer, with a creative eye for uniqueness. I am a wonderful team player full of ideas. 
             </p>
         </Container>
