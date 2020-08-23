224958105dea7b2fdc67d76017f87bc0e1e7f3e4
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-08-23 12:40:12 -0700

    changed contact page and fomatted files

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,19 +7,18 @@ function Projects() {
     return (
         <div id="projects">
             {projects.map(project => (
-            <Portfolio
-                    id= {project.id}
-                    key= {project.id}
-                    project= {project.project}
-                    src= {project.src}
-                    alt= {project.alt}
-                    depl= {project.depl}
-                    git= {project.git}
-            />
+                <Portfolio
+                    id={project.id}
+                    key={project.id}
+                    project={project.project}
+                    src={project.src}
+                    alt={project.alt}
+                    depl={project.depl}
+                    git={project.git}
+                />
             ))}
-           
         </div>
     );
-    }
+}
 
 export default Projects;
