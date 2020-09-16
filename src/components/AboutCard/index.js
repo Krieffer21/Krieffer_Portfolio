fc13f108a260e178b66ab0a587bbed6b74947033
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-16 13:49:56 -0700

    restyling and editing about me page

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -4,26 +4,32 @@ import Container from "../Container"
 
 function AboutCard() {
     return (
-        <div>
+        <div className="bg-img">
             <div className="row justify-content-md-center">
                 <Container id="about" className="col-md-4 col-sm-12 col-lg-4">
-                    <h1> About Me </h1>
-                    <img className="meimg" alt="Kaelyn Rieffer" src="https://drive.google.com/uc?export=view&id=1YcMD9rA503W7n0WWbZk4DxeKdCVELjey" />
-                    <p className="meText"> I am an artist and aspiring web developer, with a creative eye for uniqueness. I am a wonderful team player full of ideas.
+                    <img className="meimg mt-2 mb-1" alt="Kaelyn Rieffer" src="https://drive.google.com/uc?export=view&id=1YcMD9rA503W7n0WWbZk4DxeKdCVELjey" />
+                    <p className="meText mt-1 mb-1"> I am an artist and aspiring web developer, with a creative eye for uniqueness. I am a wonderful team player full of ideas.
                     </p>
                 </Container>
             </div>
-            <section id="box">
-                <div className="container link">
-                    <h3> Links </h3>
-                    <a type="submit" href="https://github.com/Krieffer21/">Git Hub Page: https://github.com/Krieffer21/ </a>
-                    <Container>
-                    <a className="container" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">LinkedIn page:
-                    https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/ </a>
-                    <a className="container" type="submit" href="https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf"> Resume: https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf</a>
-                    </Container>
-                </div>
-            </section>
+            <div className="row justify-content-md-center">
+
+                <Container className="col-md-4 col-sm-12 col-lg-4">
+                    <section id="box">
+                        <div className="container link">
+                            <h3 className="links"> Links </h3>
+                            <a className="container" type="submit" href="https://github.com/Krieffer21/">
+                                <button>Git Hub Page</button>
+                            </a>
+                            <a className="container" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">                        <button>LinkedIn page</button>
+                            </a>
+                            <a className="container" type="submit" href="https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf">
+                            <button>Resume</button>
+                            </a>
+                        </div>
+                    </section>
+                </Container>
+            </div>
         </div>
     );
 }
