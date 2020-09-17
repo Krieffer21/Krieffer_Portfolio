3be025a4b771fd37c20b07bfa27429631da80a39
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-16 17:15:27 -0700

    crop bg img adding button hover

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -15,17 +15,17 @@ function AboutCard() {
             </div>
             <div className="row justify-content-md-center">
 
-                <Container className="col-md-4 col-sm-12 col-lg-4">
+                <Container className="col-md-6 col-sm-8 col-lg-6">
                     <section id="box">
                         <div className="container link">
                             <h3 className="links"> Links </h3>
                             <a className="container" type="submit" href="https://github.com/Krieffer21/">
-                                <button>Git Hub Page</button>
+                                <button className="link-btns">Git Hub Page</button>
                             </a>
-                            <a className="container" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">                        <button>LinkedIn page</button>
+                            <a className="container" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">                        <button className="link-btns">LinkedIn page</button>
                             </a>
                             <a className="container" type="submit" href="https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf">
-                            <button>Resume</button>
+                            <button className="link-btns">Resume</button>
                             </a>
                         </div>
                     </section>
