18aa43d02b3b80bb518409588cb22f52cbc8f398
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-17 16:25:07 -0700

    better layout still not there, good mobile

--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -5,20 +5,20 @@ import Container from "../Container"
 
 function Portfolio(props) {
     return (
-        <div className="row box" >
-            <div className="col-lg-8 col-md-6 col-sm-12">
-            <section className="portfolio ">
-                <h4> {props.project} </h4>
-                <article className="container port">
-                    <img className="project" alt={props.alt} src={props.src} />
-                         <div>{props.about}</div>
-                    <Container>
-                        <a className="container" type="submit" href={props.depl}> <button className="btn-links">Deployed link</button></a>
-                        <a className="container" type="submit" href={props.git}> <button className="btn-links">GitHub link</button></a>
-                    </Container>
-                </article>
-            </section>
-            </div>
+        <div className="box col-lg-4 col-md-6 col-sm-12 mx-auto">
+            <Container>
+                <section className="portfolio">
+                    <h4> {props.project} </h4>
+                    <article className="container port">
+                        <img className="project" alt={props.alt} src={props.src} />
+                        <div>{props.about}</div>
+                        <Container>
+                            <a className="container" type="submit" href={props.depl}> <button className="btn-links">Deployed link</button></a>
+                            <a className="container" type="submit" href={props.git}> <button className="btn-links">GitHub link</button></a>
+                        </Container>
+                    </article>
+                </section>
+            </Container>
         </div>
     );
 }
