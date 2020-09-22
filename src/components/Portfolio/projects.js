dafe8d365d44dc2f8b49a829a8ecdf2c6460d07a
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-22 16:53:56 -0700

    styled cards

--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -1,40 +1,19 @@
 import React from "react";
 import "./style.css";
-import Container from "../Container"
-
 
 function Portfolio(props) {
     return (
         <div className="portfolio">
-                <div className="card d-flex">
-                    <img className="project" alt={props.alt} src={props.src} />
-                    <div className="card-body">
-                        <h5 className="card-title">{props.project}</h5>
-                        <p className="card-text aboutpro">{props.about}</p>
-                        <a href={props.depl} class="btn btn-primary linkbtns">Deployed link</a>
-                        <a href={props.git} class="btn btn-primary linkbtns">GitHub link</a>
+            <div className="card port">
+                <img className="project" alt={props.alt} src={props.src} />
+                <div className="card-body">
+                    <h5 className="card-title">{props.project}</h5>
+                    <p className="card-text aboutpro">{props.about}</p>
+                    <a href={props.depl} className="btn btn-primary linkbtns btn-links">Deployed link</a>
+                    <a href={props.git} className="btn btn-primary linkbtns btn-links">GitHub link</a>
                 </div>
             </div>
-
-            {/* <div className="contact">
-                <Container>
-                    <div className="row">
-                        <section className="portfolio col-lg-6">
-                            <h4> {props.project} </h4>
-                            <article className="port d-flex">
-                                <img className="project" alt={props.alt} src={props.src} />
-                                <div className="aboutpro">{props.about}</div>
-                                <a className="linkbtns" type="submit" href={props.depl}> <button className="btn-links">Deployed link</button></a>
-                                <a className="linkbtns" type="submit" href={props.git}> <button className="btn-links">GitHub link</button></a>
-                            </article>
-                        </section>
-                    </div>
-                </Container>
-            </div> */}
-
         </div>
-
-
     );
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,11 +3,14 @@ import Portfolio from "../Portfolio";
 
 function PortfolioPage(props) {
   return (
-    <div className="row">
-      <div className="col-lg-6 col-md-4 col-sm-12">
-        <Portfolio {...props.children} />
+          <div className="container-fluid">
+
+    <div className="row justify-content-md-center">
+        <div className="col-lg-6 col-md-10 col-sm-12">
+          <Portfolio {...props.children} />
+        </div>
       </div>
-    </div>
+     </div>
   );
 }
 
