151c88a911584b8e29d73ebbe0bb01afb4533a85
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-09-25 15:05:44 -0700

    styled about page and links

--- client/src/components/Links/index.js
+++ client/src/components/Links/index.js
@@ -4,23 +4,20 @@ import Container from "../Container";
 
 function Link() {
     return (
-        <div className="row justify-content-md-center mt-3">
-            <Container className="col-md-6 col-sm-8 col-lg-6">
-                <section id="box">
-                    <div className="container link">
-                        <h3 className="links"> Links </h3>
-                        <a className="container lbtns" type="submit" href="https://github.com/Krieffer21/">
+        <div className="row justify-content-md-center ">
+                <section id="box" className="">
+                    <div className=" link">
+                        <a className=" lbtns" type="submit" href="https://github.com/Krieffer21/">
                             <button className="link-btns">Git Hub Page</button>
                         </a>
-                        <a className="container lbtns" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">
+                        <a className=" lbtns" type="submit" href="https://www.linkedin.com/in/kaelyn-rieffer-b41a811a2/">
                             <button className="link-btns">LinkedIn page</button>
                         </a>
-                        <a className="container lbtns" type="submit" href="https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf">
+                        <a className=" lbtns" type="submit" href="https://docs.google.com/document/d/1eRAWQx83EC40dg7i9n2EdPf5q38tbL7Lx2Y2ExZxBAQ/export?format=pdf">
                             <button className="link-btns">Resume</button>
                         </a>
                     </div>
                 </section>
-            </Container>
         </div>
     );
 }
